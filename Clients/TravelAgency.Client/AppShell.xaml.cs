8f335c712cf553c8a9b3da50e263ba10041b2a70
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-04-04 18:47:06 +0200

    [ADD] .NET MAUI client app

---
+++ Clients/TravelAgency.Client/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelAgency.Client
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Clients/TravelAgency.Client/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TravelAgency.Client
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
