45b033c948de3f23d26c270dbfb49ed1d89763ca
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-06-12 13:36:56 +0200

    [ADD] Fixed ApiConfig

--- Clients/TravelAgency.Client/ApiConfig.cs
+++ Clients/TravelAgency.Client/ApiConfig.cs
@@ -3,8 +3,10 @@
     public class ApiConfig
     {
 #if DEBUG
-        public const string ApiUrl = "https://travelagencyavans.azurewebsites.net";//"https://travelagency.local:7070";
-        public const string ApiTestAuthenticatedUrl = "https://travelagencyavans.azurewebsites.net/api/account/profile";//"https://travelagency.local:7070/api/account/profile";
+        public const string ApiUrl = "https://travelagency.local:7070";
+        public const string ApiTestAuthenticatedUrl = "https://travelagency.local:7070/api/account/profile";
+        //public const string ApiUrl = "https://travelagencyavans.azurewebsites.net";
+        //public const string ApiTestAuthenticatedUrl = "https://travelagencyavans.azurewebsites.net/api/account/profile";
         public const string ClientId = "TravelAgency.Client";
         public const string Scopes = "openid offline_access profile TravelAgencyAPI";
         public const string CustomProtocolRedirectUri = "travelagency://callback";
